92dde34597367e50c00fab1d825ac3c6657ed58b
Vyacheslav Cherkashin <v.cherkashin@samsung.com>
2012-11-29 10:04:48 +0400

    ... %) ... fix 875e37afe61583c4a0901ec8482ec4aef1694328

--- kprobe/dbi_kprobes.c
+++ kprobe/dbi_kprobes.c
@@ -860,7 +860,7 @@ void patch_suspended_all_task_ret_addr(struct kretprobe *rp)
 #ifdef CONFIG_X86
 	/* workaround for do_exit probe on x86 targets */
 	if ((current->flags & PF_EXITING) || (current->flags & PF_EXITPIDONE)) {
-		patch_suspended_task_ret_addr(current, sched_rp);
+		patch_suspended_task_ret_addr(current, rp);
 	}
 #endif
 	rcu_read_unlock();
