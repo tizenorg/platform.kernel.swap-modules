0101dfc1650e1537bf37dd3c2a96db1821d8eb05
Vyacheslav Cherkashin <v.cherkashin@samsung.com>
2013-03-15 17:59:14 +0400

    [REFACTOR] create longjmp_break_uhandler()

--- uprobe/arch/asm-arm/swap_uprobes.h
+++ uprobe/arch/asm-arm/swap_uprobes.h
@@ -8,6 +8,10 @@ struct task_struct;
 int arch_prepare_uprobe(struct kprobe *p, struct task_struct *task, int atomic);
 
 int setjmp_upre_handler(struct kprobe *p, struct pt_regs *regs);
+static inline int longjmp_break_uhandler(struct kprobe *p, struct pt_regs *regs)
+{
+	return 0;
+}
 
 int swap_arch_init_uprobes(void);
 void swap_arch_exit_uprobes(void);
