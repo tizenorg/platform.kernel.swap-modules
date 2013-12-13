1155edc1248d378cd9edac2add304b1cc9fddae8
Vyacheslav Cherkashin <v.cherkashin@samsung.com>
2013-12-13 20:52:49 +0400

    [REFACTOR] remove dead code
    in pack_args()
    
    Change-Id: Ic9add40b752380681536f6431422694afb7ad1ad
    Signed-off-by: Vyacheslav Cherkashin <v.cherkashin@samsung.com>

--- writer/swap_writer_module.c
+++ writer/swap_writer_module.c
@@ -560,15 +560,11 @@ static int pack_args(char *buf, int len, const char *fmt, struct pt_regs *regs)
 
 		switch (fmt[fmt_i]) {
 		case 'b': /* 1 byte(bool) */
-			if (len < 1)
-				return -ENOMEM;
 			*buf = (char)!!get_arg(regs, i);
 			buf += 1;
 			len -= 1;
 			break;
 		case 'c': /* 1 byte(char) */
-			if (len < 1)
-				return -ENOMEM;
 			*buf = (char)get_arg(regs, i);
 			buf += 1;
 			len -= 1;
