6d80523731effe3557f68f6ac9fb9c0c2b72e927
Vitaliy Cherepanov <v.cherepanov@samsung.com>
2014-07-18 13:48:41 +0400

    [FIX] daemon start for buildin modules
    Change-Id: I25ad3bc176acee354d74318d13e7b8bf5482be98
    Signed-off-by: Vitaliy Cherepanov <v.cherepanov@samsung.com>

--- start.sh
+++ start.sh
@@ -1,18 +1,21 @@
 #!/bin/sh
 
-insmod swap_buffer.ko || exit 1  # buffer is loaded
-insmod swap_ksyms.ko || exit 1
-insmod swap_driver.ko || exit 1  # driver is loaded
-insmod swap_writer.ko || exit 1
-insmod swap_kprobe.ko || exit 1  # kprobe is loaded
-insmod swap_ks_manager.ko || exit 1  # ks_manager is loaded
-insmod swap_uprobe.ko || exit 1  # uprobe is loaded
-insmod swap_us_manager.ko || exit 1  # us_manager is loaded
-insmod swap_ks_features.ko || exit 1  # ks_features is loaded
-insmod swap_sampler.ko || exit 1
-insmod swap_energy.ko || exit 1
-insmod swap_message_parser.ko || exit 1  # parser is loaded
+if [ ! -e /sys/kernel/debug/swap/writer/raw ]; then
 
+    insmod swap_buffer.ko || exit 1  # buffer is loaded
+    insmod swap_ksyms.ko || exit 1
+    insmod swap_driver.ko || exit 1  # driver is loaded
+    insmod swap_writer.ko || exit 1
+    insmod swap_kprobe.ko || exit 1  # kprobe is loaded
+    insmod swap_ks_manager.ko || exit 1  # ks_manager is loaded
+    insmod swap_uprobe.ko || exit 1  # uprobe is loaded
+    insmod swap_us_manager.ko || exit 1  # us_manager is loaded
+    insmod swap_ks_features.ko || exit 1  # ks_features is loaded
+    insmod swap_sampler.ko || exit 1
+    insmod swap_energy.ko || exit 1
+    insmod swap_message_parser.ko || exit 1  # parser is loaded
+
+fi
 
 # Energy coefficients
 # CPU coefficients are divided by 10^6 because
