ad7f8649859b1fd6de874b99c2853de019651836
Vyacheslav Cherkashin <v.cherkashin@samsung.com>
2015-09-24 21:47:57 +0300

    [FIX] use d_path between path_get()/path_put()
    Change-Id: I608bca68742aacdba4456f772cb2e006fc65b72c
    Signed-off-by: Vyacheslav Cherkashin <v.cherkashin@samsung.com>

--- ks_features/file_ops.c
+++ ks_features/file_ops.c
@@ -347,7 +347,11 @@ static int fops_fcheck(struct task_struct *task, struct file *file)
 
 static char *fops_fpath(struct file *file, char *buf, int buflen)
 {
-	char *filename = d_path(&file->f_path, buf, buflen);
+	char *filename;
+
+	path_get(&file->f_path);
+	filename = d_path(&file->f_path, buf, buflen);
+	path_put(&file->f_path);
 
 	if (IS_ERR_OR_NULL(filename)) {
 		printk(FOPS_PREFIX "d_path FAILED: %ld\n", PTR_ERR(filename));
